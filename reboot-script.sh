9ffd2f138899538ab14ea9da6f6dd88c2de4a5d4
Justin Ortiz <jortiz@coralmac115.avtr.net>
2025-02-26 14:48:41 -0800

    reverts to Ryan's script

--- reboot-script.sh
+++ reboot-script.sh
@@ -1,33 +1,5 @@
 #!/bin/bash
 
-# Detect the operating system
-os_name=$(uname -s)
-
-# Determine the venv activation command
-if [ "$os_name" == "Darwin" ]; then 
-    venv_activate="source /Volumes/proj/avtr/work/jortiz/reboot-script/.venv/bin/activate"
-elif [ "$os_name" == "Linux" ]; then
-    venv_activate="source /proj/avtr/work/jortiz/reboot-script/.venv/bin/activate"
-else
-    echo "Unsupported operating system."
-    exit 1
-fi
-
-# Activate the virtual environment
-eval "$venv_activate"
-
-if [ "$os_name" == "Darwin" ]; then
-    # download libs to venv
-    pip3 install -r /Volumes/proj/avtr/work/jortiz/reboot-script/requirements.txt
-    # Run the Python script with arguments
-    python3 /Volumes/proj/avtr/work/jortiz/reboot-script/main.py "$@"
-
-else
-    # download libs to venv
-    pip3 install -r /proj/avtr/work/jortiz/reboot-script/requirements.txt
-    # Run the Python script with arguments
-    python3 /proj/avtr/work/jortiz/reboot-script/main.py "$@"
-fi
-
-# Deactivate the virtual environment (optional)
-deactivate
+BASE_DIR=$(dirname $(realpath $0))
+source ${BASE_DIR}/.venv/bin/activate
+python ${BASE_DIR}/main.py ${@}
