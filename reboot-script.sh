fe94e37936481e603041cee600fe18e395349a0d
Justin Ortiz <jortiz@coralmac115.avtr.net>
2025-02-26 12:49:22 -0800

    Updates script to work on linux and mac

--- reboot-script.sh
+++ reboot-script.sh
@@ -5,7 +5,7 @@ os_name=$(uname -s)
 
 # Determine the venv activation command
 if [[ "$os_name" == "Darwin" || "$os_name" == "Linux" ]]; then
-    venv_activate="source .venv/bin/activate"
+    venv_activate="source /Volumes/proj/avtr/work/jortiz/reboot-script/.venv/bin/activate"
 else
     echo "Unsupported operating system."
     exit 1
@@ -14,11 +14,18 @@ fi
 # Activate the virtual environment
 eval "$venv_activate"
 
-# download libs to venv
-pip3 install -r requirements.txt
+if [ "$os_name" == "Darwin" ]; then
+    # download libs to venv
+    pip3 install -r /Volumes/proj/avtr/work/jortiz/reboot-script/requirements.txt
+    # Run the Python script with arguments
+    python3 /Volumes/proj/avtr/work/jortiz/reboot-script/main.py "$@"
 
-# Run the Python script with arguments
-python3 main.py "$@"
+else
+    # download libs to venv
+    pip3 install -r /proj/avtr/work/jortiz/reboot-script/requirements.txt
+    # Run the Python script with arguments
+    python3 /proj/avtr/work/jortiz/reboot-script/main.py "$@"
+fi
 
 # Deactivate the virtual environment (optional)
 deactivate
